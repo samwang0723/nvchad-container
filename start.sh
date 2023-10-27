68b502dfb1e2a861d722b90dd8875d910e74e85e
Sam Wang <sam.wang.0723@gmail.com>
2023-10-27 08:35:03 +0800

    feat: support launch with customized image

--- start.sh
+++ start.sh
@@ -18,7 +18,11 @@ if ! docker image inspect nvchad-base >/dev/null 2>&1; then
 	# If package still not able to install like stylua, you can install it manually.
 	# cargo install stylua
 else
+	# After setup all environment, you can commit into a new image
+	# docker commit -a 'sam.wang.0723@gmail.com' -m 'NvChad cloud container'  3779db8b72ac nvchad-base:v2
+	# docker run -it -v .:/workspace --name nvchad-container-v2 nvchad-base:v2 /bin/bash '-l'
+
 	# If the image already exists, just run it
-	docker start nvchad-container
-	docker exec -it nvchad-container bash
+	docker start nvchad-container-v2
+	docker exec -it nvchad-container-v2 bash '-l'
 fi
