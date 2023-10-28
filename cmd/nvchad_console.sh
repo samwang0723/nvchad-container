d62e02ea07ee9637caac7c8fd8c4a4a4cab558a8
Sam Wang <sam.wang.0723@gmail.com>
2023-10-28 10:23:31 +0800

    feat: update using zsh

--- cmd/nvchad_console.sh
+++ cmd/nvchad_console.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 if ! docker container inspect nvchad-container-v1 >/dev/null 2>&1; then
-	# Create container from customized image
-	docker run -it --platform linux/amd64 --cpus 2 -v .:/workspace --name nvchad-container-v1 samwang0723/nvchad-base:v1.0.1 /bin/bash '-l'
+	docker volume create nvchad-volume
+	# Create container from customized image, if not installed the zsh, please use bash '-l'
+	docker run -it --platform linux/amd64 --cpus 2 -v nvchad-volume:/workspace --name nvchad-container-v1 samwang0723/nvchad-base:v1.2.0 zsh
 else
 	# Start the container
 	docker start nvchad-container-v1
 	# Set container CPU
 	docker update --cpus 2 nvchad-container-v1
-	# Attach to the container
-	docker exec -it nvchad-container-v1 bash '-l'
+	# Attach to the container, if not installed the zsh, please use bash '-l'
+	docker exec -it nvchad-container-v1 zsh
 fi
