da2e9c9598afb898c4e92c74ba665e4c5a0276d4
Sam Wang <sam.wang.0723@gmail.com>
2024-05-24 23:31:32 +0800

    fix: solve highlight issue

--- custom/init.lua
+++ custom/init.lua
@@ -8,3 +8,29 @@ vim.api.nvim_create_autocmd("BufReadPre", { pattern = "/private/*", command = "s
 vim.opt.undofile = true
 vim.o.shell = "/bin/zsh"
 vim.opt.clipboard = ""
+
+-- Use Lua to set up the key mapping
+vim.api.nvim_set_keymap("n", "y", '"+y', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("x", "y", '"+y', { noremap = true, silent = true })
+
+vim.cmd [[
+nnoremap z/ :if AutoHighlightToggle()<Bar>set hls<Bar>endif<CR>
+function! AutoHighlightToggle()
+    let @/ = ''
+    if exists('#auto_highlight')
+        au! auto_highlight
+        augroup! auto_highlight
+        setl updatetime=4000
+        echo 'Highlight current word: off'
+        return 0
+    else
+        augroup auto_highlight
+            au!
+            au CursorHold * let @/ = '\V\<'.escape(expand('<cword>'), '\').'\>'
+        augroup end
+        setl updatetime=10
+        echo 'Highlight current word: ON'
+        return 1
+    endif
+endfunction
+]]
